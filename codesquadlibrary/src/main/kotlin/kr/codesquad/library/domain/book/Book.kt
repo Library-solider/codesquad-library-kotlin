fd9b2f71b4538f088e685b34b4638e8ea1773f65
Sunny <thstkd92@gmail.com>
2022-07-03 23:31:13 +0900

    [feat] Book entity 추가

---
+++ codesquadlibrary/src/main/kotlin/kr/codesquad/library/domain/book/Book.kt
@@ -0,0 +1,36 @@
+package kr.codesquad.library.domain.book
+
+import kr.codesquad.library.domain.bookcase.Bookcase
+import kr.codesquad.library.domain.category.Category
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+class Book(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    var id: Long? = null,
+
+    var title: String,
+    var description: String? = null,
+    var author: String? = null,
+    var publisher: String? = null,
+
+    @Column(columnDefinition = "TIMESTAMP")
+    var publicationDate: LocalDate? = null,
+
+    var isbn: String? = null,
+    var available: Boolean = true,
+    var recommendCount: Int = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    var category: Category? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bookcase_id")
+    var bookcase: Bookcase? = null,
+) {
+}
