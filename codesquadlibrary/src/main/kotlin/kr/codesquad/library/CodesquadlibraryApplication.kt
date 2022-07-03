1038c231bd7a2150ed9d2577d654514cd1909735
Sunny <thstkd92@gmail.com>
2022-07-03 15:32:04 +0900

    [chore] kotlin spring init

---
+++ codesquadlibrary/src/main/kotlin/kr/codesquad/library/CodesquadlibraryApplication.kt
@@ -0,0 +1,11 @@
+package kr.codesquad.library
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodesquadlibraryApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodesquadlibraryApplication>(*args)
+}
